b82599a9ecbe1e35c23552c71cac7cd7e1b0fa30
Sean Cross <sean@xobs.io>
2023-10-20 16:55:49 +0800

    remove upstream
    Signed-off-by: Sean Cross <sean@xobs.io>

--- build.rs
+++ build.rs
@@ -2,30 +2,20 @@ extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
-use std::process::Command;
 
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     println!("cargo:rustc-link-search={:?}", out_path);
 
-    let _output = Command::new("git")
-        .arg("submodule")
-        .arg("update")
-        .arg("--init")
-        .arg("--recursive")
-        .arg("--remote")
-        .output()
-        .expect("Failed to execute git command.");
-
     // build a static lib
     cc::Build::new()
-        .file("src/rpi_ws281x/mailbox.c")
-        .file("src/rpi_ws281x/ws2811.c")
-        .file("src/rpi_ws281x/pwm.c")
-        .file("src/rpi_ws281x/pcm.c")
-        .file("src/rpi_ws281x/dma.c")
-        .file("src/rpi_ws281x/rpihw.c")
+        .file("src/upstream/mailbox.c")
+        .file("src/upstream/ws2811.c")
+        .file("src/upstream/pwm.c")
+        .file("src/upstream/pcm.c")
+        .file("src/upstream/dma.c")
+        .file("src/upstream/rpihw.c")
         // create a static lib to make cross-compiling
         // and uploading easier.
         .shared_flag(false)
